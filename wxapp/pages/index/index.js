8b3dfb143bc7c0fe31147fd707650669379d70fd
Elisha Kingdom <elishakingdom@gmail.com>
2021-12-18 20:34:24 +0800

    updated the UI to use English on homepage

--- wxapp/pages/index/index.js
+++ wxapp/pages/index/index.js
@@ -57,7 +57,7 @@ Page({
     onShareAppMessage: function () {
         let info = wx.getStorageSync('userInfo');
         return {
-            title: '海风小店',
+            title: 'WineAndDine',
             desc: '开源微信小程序商城',
             path: '/pages/index/index?id=' + info.id
         }
